87ec77955462898908c8e631aaf4520f68c9e90b
LFLucas <132351795+LFLucas@users.noreply.github.com>
2023-05-07 17:24:03 -0300

    novo commit

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class ContentComponent {
   contentText = "Um aplicativo que possibilite a identificação e localização de pontos de coleta de agasalhos em diversas regiões pode ser bastante útil para incentivar a sociedade a doar. Com esse tipo de app, os usuários vão encontrar com facilidade os locais de arrecadação mais próximos de sua localidade"
-  contentText1 = ""
+  contentText1 = "O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500, quando uma misturou os caracteres de um texto para criar um espécime de livro. Este texto não só sobreviveu 5 séculos, mas também o salto para a tipografia electrónica, mantendo-se essencialmente inalterada. Foi popularizada nos anos 60 com a disponibilização das folhas de Letraset, que continham passagens com Lorem Ipsum, e mais recentemente com os programas de publicação como o Aldus PageMaker que incluem versões do Lorem Ipsum."
 }
